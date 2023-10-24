4ea57507137c4dddab95e2d643ee5160457de4b2
gastondandrea <gastondandrea@hotmail.com>
2023-10-24 00:28:17 -0300

    Termiando ejercicios

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,47 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+// INPUT "101" -> OUTPUT num 34
+function BinarioADecimal(string) {
+   /*
+   var num = parseInt(string, 2);
+   return num;
+   */
+   /*let decimal = 0;
+   let array = string.split("").reverse()
+   array.forEach(function (element, index) {
+      decimal = decimal + Math.pow(2, index) * element
+   });
+   return decimal;*/
+ 
+   var array = string.split("").reverse().map((letra)=>{
+     return parseInt(letra);
+   });
+   var aux = 0;
+   var result = array.reduce((suma, elemnet, index)=>{
+       suma += elemnet*(2**index);
+       return suma;
+      },0)
+   return result;
+ }
+ 
 
-function DecimalABinario(num) {}
+console.log(BinarioADecimal('101'));
+
+// INPUT num 8 -> OUTPUT '1000'
+function DecimalABinario(num) {
+   /*
+   var string = num.toString(2);
+   return string;
+   */
+   var array = [];
+   while(num!=0){
+     array.push(num%2);
+     num = Math.trunc(num/2);
+   }
+   return array.reverse().join('');
+   
+ }
+console.log(DecimalABinario(10));
 
 module.exports = {
    BinarioADecimal,
