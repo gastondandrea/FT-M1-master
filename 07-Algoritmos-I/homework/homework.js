8cfc117ad39a585cf848846c0afb8fe9c3ffe44c
gastondandrea <gastondandrea@hotmail.com>
2023-11-05 21:37:50 -0300

    Homework finalizada

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,35 +6,76 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  var result = [1];
+  var baseDiv = 2;
+  while(num>1){
+    if(num % baseDiv === 0){
+      result.push(baseDiv);
+      num = num / baseDiv;
+    }else{
+      baseDiv++;
+    }
+  }
+  return result;
 }
 
-function bubbleSort(array) {
+function bubbleSort(array) {                // Compara con el valor siguiente y ordena el array
   // Implementar el método conocido como bubbleSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  var swap = true;
+  while(swap){
+    swap = false;
+    for (let i = 0; i < array.length - 1; i++) {
+      if(array[i] > array[i + 1]){
+        var aux = array[i];
+        array[i] = array[i+1];
+        array[i+1] = aux;
+        swap = true;
+      }
+    }
+  }
+  return array;
 }
 
 
-function insertionSort(array) {
+function insertionSort(array) {               // Compara con el valor anterior y ordena el array
   // Implementar el método conocido como insertionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j >= 1; j--) {
+      if(array[j] < array[j - 1]){
+        var aux = array[j];
+        array[j] = array[j-1];
+        array[j-1] = aux;
+      }
+    }
+  }
+  return array;
 }
 
 
-function selectionSort(array) {
+function selectionSort(array) {             //
   // Implementar el método conocido como selectionSort para ordenar de menor a mayor
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for (let i = 0; i < array.length -1; i++) {
+    for (let j = i+1; j < array.length; j++) {
+      if(array[j]>array[i]){
+        var aux = array[i];
+        array[i] = array[j];
+        array[j] = aux;
+      }
+    }
+  }
+  return array;
 }
-
+let array = [3,5,8,7]
+console.log(selectionSort(array));
 
 // No modificar nada debajo de esta línea
 // --------------------------------
--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -1,21 +1,67 @@
 'use strict'
 // No cambies los nombres de las funciones.
-
+/*
+01 extraer pivot
+02 divide y ordena en [] left y [] right
+03 aplica recursion con [].length > 1 <- caso base es [].length <= 1
+04 concat y listo
+*/ 
 function quickSort(array) {
   // Implementar el método conocido como quickSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  if(array.length <= 1) return array // caso corte recursion
+  var pivot = Math.floor(array.length / 2); //es el index (posicion) en el centro <- array[pivot]
+  var left = [];
+  var right = [];
+  for (let i = 0; i < array.length; i++) {
+    if(i !== pivot){
+      if(array[i] <= array[pivot]){
+        left.push(array[i]);
+      }else{
+        right.push(array[i]);
+      }
+    }
+  }
+  //aplicamos recursion y concatenamos los arrays y el pivot.
+  return quickSort(left).concat(array[pivot]).concat(quickSort(right));
 }
 
-function mergeSort(array) {
+
+/*
+01 divide recursiva hasta que c/elem es un []
+02 ahora merge y donde tb compara y ordena
+*/ 
+
+function mergeSort(list) {
   // Implementar el método conocido como mergeSort para ordenar de menor a mayor
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
 
-  //okey probemos ahora
+  if(list.length <= 1)return list; // caso corte recursion
+  var lineDiv = Math.floor(list.length/2); // tomamos la linea para dividir
+  console.log(lineDiv);
+
+  var leftArray  = list.slice(0, lineDiv); // dividioms el array en left
+  let rightArray = list.slice(lineDiv); // dividimos la otra mitad en right
+  console.log(leftArray);
+  console.log(rightArray);
+
+  leftArray = mergeSort(leftArray); // hacemos recursion en la mitad 
+  rightArray = mergeSort(rightArray); // hacemos recursion en la otra mitad
+
+  var arrayResult = []; 
+  while(leftArray.length && rightArray.length){   // si tiene valor a la derecha y a la izquierda
+    if(leftArray[0] < rightArray[0]){ // compara quien es menor
+      arrayResult.push(leftArray.shift()); // saca el elemento menor del array y lo pone en arrayResult
+    }else{
+      arrayResult.push(rightArray.shift()); // saca el elemento menor del array y lo pone en arrayResult
+    }
+  }
+  arrayResult = arrayResult.concat(leftArray, rightArray); // concatena todo
+  return arrayResult; //listo..
 }
 
 // No modificar nada debajo de esta línea
